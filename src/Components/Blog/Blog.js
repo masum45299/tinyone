fe342a2b3cd39e2f22d1893b585cb6fdce1c5229
masum45299 <masum459299@gmail.com>
2023-03-22 16:42:47 +0600

    update

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>This is Blog page</h1>
+            <h1 className='text-center'>Sorry !! working page</h1>
         </div>
     );
 };
--- src/Components/Support/Support.js
+++ src/Components/Support/Support.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Support = () => {
     return (
         <div>
-            <h1>This is Support page</h1>
+            <h1 className='text-center'>Sorry !! working page</h1>
         </div>
     );
 };
